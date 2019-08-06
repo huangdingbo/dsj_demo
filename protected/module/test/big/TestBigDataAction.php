1da0a99831a6dc607e4d4d9fe8fd61b29060a396
huangdingbo <hdjVERj3b>
2019-08-06 15:24:02 +0800

    初始

--- protected/module/test/big/TestBigDataAction.php
+++ protected/module/test/big/TestBigDataAction.php
@@ -5,12 +5,16 @@ namespace module\test\big;
 
 
 use CC\db\base\select\ListModel;
+use components\layui\MenuWidget;
 use CRequest;
 
 class TestBigDataAction extends \CAction
 {
     public function execute(CRequest $request)
     {
+        $val = MenuWidget::test();
+
+        var_dump($val);exit;
         $list = ListModel::make('case_record')
             ->limit(100)
             ->execute();
